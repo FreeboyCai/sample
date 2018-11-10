4654afe72ebbf671448c4a33c8db32189ebdc6a4
vagrant <vagrant@homestead>
2018-11-10 07:39:05 +0000

    Finish user edit,update,index,and destory actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,12 @@ use Auth;
 
 class SessionsController extends Controller
 {
+   
+    public function __construct(){
+
+        $this->middleware('guest',['only'=>['create']]);
+    }
+
     public function create(){
     	return view('sessions.create');
     }
@@ -19,7 +25,7 @@ class SessionsController extends Controller
     	]);
     	 if (Auth::attempt($credentials, $request->has('remember'))) {
     		session()->flash('success','欢迎回来！');
-    		return redirect()->route('users.show',[Auth::user()]);
+    		return redirect()->intended(route('users.show',[Auth::user()]));
     	}else{
     		session()->flash('danger','抱歉，用户名或密码错误！');
     		return redirect()->back();
